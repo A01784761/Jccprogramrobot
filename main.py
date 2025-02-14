c0a79d5a123ebe4fd4a0c8e6cba2f6ea41dc6a42
A01784761 <A01784761@tec.mx>
2025-02-13 20:11:29 -0600

    Update main.py
    Agregué fórmula de volumen

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ def calcular_perimetro_area():
     radio = float(input("Introduce el radio del círculo: "))
     perimetro = 2 * math.pi * radio
     area = math.pi * radio ** 2
+    altura = float(input("Introduce la altura del cilindro: "))
+    volumen = area * altura
+
+    print(f"El volumen del cilindro es: {volumen}")
 
     print(f"El perímetro del círculo es: {perimetro}")
     print(f"El área del círculo es: {area}")
